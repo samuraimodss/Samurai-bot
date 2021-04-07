674143b3772c3e06da865a9fb6c96da83b70f4f4
samuraimodss <82075336+samuraimodss@users.noreply.github.com>
2021-04-06 23:25:39 -0500

    Add files via upload

---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `Jode @${number}. Que bueno verte por aqui🥺 ${groupname} lee las reglas para no tener mal entendidos crack 😉`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Que bueno que te fuiste😵 @${number}. 😈 ya no te queríamos ojala nunca regreses👋`
+}
+exports.bye = bye
