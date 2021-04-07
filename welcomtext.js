19ba19e27972932561b2b5e71f44ecd01f46dd64
samuraimodss <82075336+samuraimodss@users.noreply.github.com>
2021-04-06 23:22:16 -0500

    Delete welcomtext.js

--- welcomtext.js
+++
@@ -1,9 +0,0 @@
-const welcome = (number, groupname) => {
-    return `Hola @${number}. Bienvenido al grupo ${groupname} lee las reglas para no tener mal entendidos`
-}
-exports.welcome = welcome
-
-const bye = (number) => {
-    return `Adios joto @${number}. 👋`
-}
-exports.bye = bye
